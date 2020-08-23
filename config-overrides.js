96fa5c8a6f15c997cf40dcc317880f228e7e38d4
Stanislau Berazouski <stanislau_berazouski@epam.com>
2020-08-24 02:23:02 +0300

    add config

--- config-overrides.js
+++ config-overrides.js
@@ -1,61 +1,30 @@
-const path = require('path')
-
-// const {
-//     override,
-// } = require('customize-cra');
+const path = require('path');
+const config = require('dotenv').config();
 
 module.exports = {
-    // The Webpack config to use when compiling your react app for development or production.
-    // webpack: function(config, env) {
-    //   // ...add your webpack config
-    //   console.log('test_path_1', path.resolve(__dirname, 'second-app/src/index.js'));
-    //   console.log('test_path_2', path.resolve(__dirname, 'node_modules/react-dev-utils/webpackHotDevClient.js'));
-    //   console.log('initial_config', config);
-    //   const newConfig = { 
-    //     ...config,
-    //     // context: path.resolve(__dirname, 'second-app/src'),
-    //     // entry: {
-    //     //     // main: path.resolve(__dirname, 'second-app/src/index.js'),
-    //     //     main: './index.js'
-    //     // }
-    //     entry: [
-    //         path.resolve(__dirname, 'node_modules/react-dev-utils/webpackHotDevClient.js'),
-    //         path.resolve(__dirname, 'second-app/src/index.js')
-    //       ]
-    //   }
-    //   return newConfig;
-    // },
-    paths: function(paths, env) {
-        console.log('paths',paths)
-        // ...add your paths config
+    paths: function (paths, env) {
+        const mapPaths = {
+            first: {
+                appBuild: path.resolve(__dirname, 'build/first-app/'),
+                appSrc: path.resolve(__dirname, 'first-app/src'),
+                appIndexJs: path.resolve(__dirname, 'first-app/src/index.js'),
+                appPublicL: path.resolve(__dirname, 'first-app/public')
+            },
+            second: {
+                appBuild: path.resolve(__dirname, 'build/second-app/'),
+                appSrc: path.resolve(__dirname, 'second-app/src'),
+                appIndexJs: path.resolve(__dirname, 'second-app/src/index.js'),
+                appPublicL: path.resolve(__dirname, 'second-app/public')
+            }
+        }
+        console.log('paths', paths)
+        console.log('process_env', process.env.APP_NAME);
+        console.log('config', config);
+
+        console.log('path_config', mapPaths[process.env.APP_NAME])
         return {
             ...paths,
-            appSrc:path.resolve(__dirname, 'second-app/src'),
-            appIndexJs: path.resolve(__dirname, 'second-app/src/index.jsx')
+            ...mapPaths[process.env.APP_NAME],
         };
     }
-  }
-
-    
-//   module.exports = override(
-//     config => {
-//       console.log(config.entry);
-//       console.log([
-//         path.resolve(__dirname, 'node_modules/react-dev-utils/webpackHotDevClient.js'),
-//         path.resolve(__dirname, 'second-app/src/index.jsx')
-//       ]);
-//       const newConfig = { 
-//         ...config,
-//         // context: path.resolve(__dirname, 'second-app/src'),
-//         // entry: {
-//         //     // main: path.resolve(__dirname, 'second-app/src/index.js'),
-//         //     main: './index.js'
-//         // }
-//         entry: [
-//             path.resolve(__dirname, 'node_modules/react-dev-utils/webpackHotDevClient.js'),
-//             path.resolve(__dirname, 'second-app/src/index.jsx')
-//           ]
-//       }
-//       return newConfig;
-//     },
-//   );
+}
---
+++ first-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FirstApp from './FirstApp';
+
+ReactDOM.render(
+    <FirstApp />,
+    document.getElementById('root')
+);
